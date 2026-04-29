eb28c401d35f5b027b258f93b6d0f1a122b25b4d
aryan_pramodkumarvarma <aryan_pramodkumarvarma@epam.com>
2026-04-29 11:28:49 +0000

    task completed

--- task08/outputs.tf
+++ task08/outputs.tf
@@ -1,13 +1,13 @@
 output "aci_fqdn" {
 
-    description = "The fully qualified domain name (FQDN) of the Azure Container Instance"
-    value       = module.aci.container_group.fqdn
-  
+  description = "The fully qualified domain name (FQDN) of the Azure Container Instance"
+  value       = module.aci.container_group.fqdn
+
 }
 
 output "aks_lb_ip" {
 
-    description = "The public IP address of the Azure Kubernetes Service (AKS) load balancer"
-    value       = data.kubernetes_service.app.status[0].load_balancer[0].ingress[0].ip
-  
+  description = "The public IP address of the Azure Kubernetes Service (AKS) load balancer"
+  value       = data.kubernetes_service.app.status[0].load_balancer[0].ingress[0].ip
+
 }
